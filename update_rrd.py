cb7b8746ebb271e8748850c5c06e833b8ed194de
Falko Zurell <falko.zurell@gmail.com>
2015-02-05 22:06:02 +0100

    first working version

--- update_rrd.py
+++ update_rrd.py
@@ -5,15 +5,21 @@ import random
 import rrdtool
 import urllib2
 import json
+import ConfigParser, os
+
+
+
+
+
  
 temperature = 0
 pressure = 0
-YOURDEVICEID = 
-ACCESS_TOKEN= 
+YOURDEVICEID = ""
+ACCESS_TOKEN = ""
 
 
-def read_metric(self, metric):
-	response = urllib2.urlopen('https://api.spark.io/v1/devices/' + self.YOURDEVICEID + '/' + metric + '?access_token=' + self.ACCESS_TOKEN)
+def read_metric(metric):
+	response = urllib2.urlopen('https://api.spark.io/v1/devices/' + YOURDEVICEID + '/' + metric + '?access_token=' + ACCESS_TOKEN)
 	html = response.read()
 	reading = json.loads(html)
 	result = reading['result']
@@ -27,12 +33,21 @@ def update_rrd(self, metric):
 	 total_output_traffic += random.randrange(1000, 3000)
 	 ret = rrdtool.update('speed.rrd','N:' + `total_input_traffic` + ':' + `total_output_traffic`);
 	 if ret:
-	 print rrdtool.error()
+	    print rrdtool.error()
 	 time.sleep(300)
 
 
 
 # main class if this file is used directly
 if __name__ == '__main__':
-	print "Temperature (Both): " + str(round(read_metric['temp'],2)) + " °C"
-#	print "Pressure (Both): " + str(round(both['pressure']/10,2)) + " hPa"
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('spark.cfg'))
+    print "Config DeviceID: " + config.get('SparkCloud', 'deviceID')
+    print "Config Access Token: " + config.get('SparkCloud', 'accessToken')
+
+    YOURDEVICEID = config.get('SparkCloud', 'deviceID')
+    ACCESS_TOKEN = config.get('SparkCloud', 'accessToken')
+    temp = read_metric("temp")
+    pressure = read_metric("pressure")
+    print "Temperature: " + str(round(temp,2)) + " C"
+    print "Pressure: " + str(round(pressure,2)) + " hPa"
