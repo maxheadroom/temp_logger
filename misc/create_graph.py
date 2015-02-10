3b2f214088e21e240c198e0b8501f0a7aae2fc01
Falko Zurell <falko.zurell@gmail.com>
2015-02-10 16:41:56 +0100

    added dusk, dawn and night indicator
    Signed-off-by: Falko Zurell <falko.zurell@gmail.com>

--- misc/create_graph.py
+++ misc/create_graph.py
@@ -1,6 +1,8 @@
 import sys
 import rrdtool
-
+import ephem
+from datetime import *
+from dateutil import tz
 
 # Graph Colors
 COLOUR1="#294CFF"
@@ -10,13 +12,68 @@ COLOUR4="#CC1414"
 COLOUR5="#000000"
 TRENDCOLOUR="#FFFF00"
 
+LAT=52.5243700
+LON=13.4105300
+
 TOOL_HOME="/home/fzurell/temp_logger"
 WEB_HOME=TOOL_HOME + "/web"
 RRDFILE=TOOL_HOME + "/climate.rrd"
 
+def compute_observer(lat, lon):
+	
+	# create a new observer object to calculate sunrise etc.
+	observer = ephem.Observer()
+	#PyEphem takes and returns only UTC times. 15:00 is noon in Fredericton
+	observer.date = datetime.now()
+	#Location of Fredericton, Canada
+	observer.lon  = str(lon) #Note that lon should be in string format
+	observer.lat  = str(lat)      #Note that lat should be in string format
 
+	#Elevation of Fredericton, Canada, in metres
+	observer.elev = 0
 
-def generate_temperature(rrdfile):
+	#To get U.S. Naval Astronomical Almanac values, use these settings
+	observer.pressure= 0
+	observer.horizon = "-0:34"
+	
+	sunrise=observer.previous_rising(ephem.Sun()) #Sunrise
+	noon   =observer.next_transit   (ephem.Sun(), start=sunrise) #Solar noon
+	sunset =observer.next_setting   (ephem.Sun()) #Sunset
+
+	# change horizon to calculate twilight and dusk
+	observer.horizon = '-6' #-6=civil twilight, -12=nautical, -18=astronomical
+
+	beg_twilight=observer.previous_rising(ephem.Sun(), use_center=True) #Begin civil twilight
+	end_twilight=observer.next_setting   (ephem.Sun(), use_center=True) #End civil twilight
+	
+	SRHOUR = int(ephem.localtime(sunrise).strftime("%H")) * 3600
+	SRMIN = int(ephem.localtime(sunrise).strftime("%M")) * 60
+	
+	SSHOUR = int(ephem.localtime(sunset).strftime("%H")) * 3600
+	SSMIN = int(ephem.localtime(sunset).strftime("%M")) * 60
+	
+	DUSKHOUR = int(ephem.localtime(end_twilight).strftime("%H")) * 3600
+	DUSKMIN = int(ephem.localtime(end_twilight).strftime("%M")) * 60
+
+	DAWNHOUR = int(ephem.localtime(beg_twilight).strftime("%H")) * 3600
+	DAWNMIN = int(ephem.localtime(beg_twilight).strftime("%M")) * 60
+	
+	SUNRISE = str(SRHOUR + SRMIN)
+	SUNSET = str(SSHOUR + SSMIN)
+	DUSK = str(DUSKHOUR + DUSKMIN)
+	DAWN = str(DAWNHOUR + DAWNMIN)
+	
+	return [SUNRISE, SUNSET, DUSK, DAWN ]
+	
+
+def generate_temperature(rrdfile, observer):
+	
+	
+	sunrise = observer[0]
+	sunset = observer[1]
+	dusk = observer[2]
+	dawn = observer[3]
+	
 
 	for sched in ['hourly','halfday' , 'daily' , 'weekly', 'monthly']:
 
@@ -51,6 +108,14 @@ def generate_temperature(rrdfile):
 		"DEF:humidity=" + rrdfile +":humidity:AVERAGE", 
 		"LINE1:temp" + COLOUR1 + ":temp \l",
 		"LINE1:dht" + COLOUR2 +":temp dht22\l",
+		"CDEF:nightplus=LTIME,86400,%,"+ sunrise + ",LT,INF,LTIME,86400,%," + sunset + ",GT,INF,UNKN,temp,*,IF,IF",
+		"CDEF:nightminus=LTIME,86400,%," + sunrise + ",LT,NEGINF,LTIME,86400,%," + sunset +",GT,NEGINF,UNKN,temp,*,IF,IF",
+		"AREA:nightplus#E0E0E0", 
+		"AREA:nightminus#E0E0E0",
+		"CDEF:dusktilldawn=LTIME,86400,%," + dawn + ",LT,INF,LTIME,86400,%," + dusk + ",GT,INF,UNKN,temp,*,IF,IF",
+		"CDEF:dawntilldusk=LTIME,86400,%," + dawn +",LT,NEGINF,LTIME,86400,%," + dusk + ",GT,NEGINF,UNKN,temp,*,IF,IF",
+		"AREA:dusktilldawn#CCCCCC",
+		"AREA:dawntilldusk#CCCCCC",
 		"LINE1:dewpoint" + COLOUR3 +":Dew Point\l",
 		"LINE1:humidity"+ COLOUR4 + ":Humidity in %\l",
 		"GPRINT:temp:LAST:                 Cur\:%5.1lf C",
@@ -58,7 +123,14 @@ def generate_temperature(rrdfile):
 		"GPRINT:tmin:MIN:Min\:%5.1lf C",
 		"GPRINT:tmax:MAX:Max\:%5.1lf C\n")
 
-def generate_pressure(rrdfile):
+def generate_pressure(rrdfile, observer):
+	
+	sunrise = observer[0]
+	sunset = observer[1]
+	dusk = observer[2]
+	dawn = observer[3]
+	
+	
 	for sched in ['hourly','halfday' , 'daily' , 'weekly', 'monthly']:
 
 		if sched == 'weekly':
@@ -85,6 +157,14 @@ def generate_pressure(rrdfile):
 		"DEF:pressure=" + rrdfile +":pressure:AVERAGE",
 		"DEF:pmin=" + rrdfile +":pressure:MIN",
 		"DEF:pmax=" + rrdfile +":pressure:MAX", 
+		"CDEF:nightplus=LTIME,86400,%,"+ sunrise + ",LT,INF,LTIME,86400,%," + sunset + ",GT,INF,UNKN,pressure,*,IF,IF",
+		"CDEF:nightminus=LTIME,86400,%," + sunrise + ",LT,NEGINF,LTIME,86400,%," + sunset +",GT,NEGINF,UNKN,pressure,*,IF,IF",
+		"AREA:nightplus#E0E0E0", 
+		"AREA:nightminus#E0E0E0",
+		"CDEF:dusktilldawn=LTIME,86400,%," + dawn + ",LT,INF,LTIME,86400,%," + dusk + ",GT,INF,UNKN,pressure,*,IF,IF",
+		"CDEF:dawntilldusk=LTIME,86400,%," + dawn +",LT,NEGINF,LTIME,86400,%," + dusk + ",GT,NEGINF,UNKN,pressure,*,IF,IF",
+		"AREA:dusktilldawn#CCCCCC",
+		"AREA:dawntilldusk#CCCCCC",
 		"LINE1:pressure" + COLOUR5 + ":Pressure \l",
 		"GPRINT:pressure:LAST:                 Cur\:%5.1lf hPa",
 		"GPRINT:pressure:AVERAGE:Avg\:%5.1lf hPa",
@@ -95,5 +175,8 @@ def generate_pressure(rrdfile):
 
 # main class if this file is used directly
 if __name__ == '__main__':
-	generate_temperature(RRDFILE)
-	generate_pressure(RRDFILE)
+	
+	
+	observer = compute_observer(LAT,LON)
+	generate_temperature(RRDFILE, observer)
+	generate_pressure(RRDFILE, observer)
